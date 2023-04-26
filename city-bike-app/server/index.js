d8fc90aac60ecc2c220b3a7151820bd89f2be9b8
Alba Castano <acastanome@gmail.com>
2023-04-26 22:08:52 +0300

    Using shorter file for initial testing of retrieving csv data through streaming. Current state: Read file and console data ok. Todo: Validate data

--- city-bike-app/server/index.js
+++ city-bike-app/server/index.js
@@ -9,6 +9,19 @@ app.get('/test', (req, res) => {
 	res.json({ message: 'Testing: Server is up' });
 });
 
+// START get data
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('./assets/journey_data/2021-01.csv')
+	.pipe(csv({}))
+	.on('data', (data) => results.push(data))
+	.on('error', (e) => console.log(e))
+	.on('end', () => console.log(results));
+// END get data
+
 server.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
 });
