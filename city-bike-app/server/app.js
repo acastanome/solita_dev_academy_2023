04e4ac33c417f70ac4fe4b6ded920136dc02cffd
Alba Castano <acastanome@gmail.com>
2023-05-03 16:14:05 +0300

    Deleted pid from stations table. Added endpoint to add new stations. Installed dotenv package

--- city-bike-app/server/app.js
+++ city-bike-app/server/app.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const { getJourneysFromFile } = require('./utils/getJourneysFromFile');
 const { getStationsFromFile } = require('./utils/getStationsFromFile');
 
+require('dotenv').config();
+
+const db = require('./db/index');
+
 const corsOptions = {
 	origin: 'http://localhost:3000',
 	credentials: true,
@@ -37,6 +41,69 @@ app.get('/stations', async (req, res) => {
 		res.status(200).json(allStations);
 	} catch (e) {
 		console.log(e);
+	}
+});
+
+app.post('/addStation', async (req, res) => {
+	const allStations = await getStationsFromFile(
+		'./assets/bike_stations_data/Helsingin_ja_Espoon_asemat_avoin.csv'
+	);
+	// const data = req.body;
+	const station = allStations[1];
+	// if (
+	// 	data.station.ID &&
+	// 	data.station.Nimi &&
+	// 	data.station.Namn &&
+	// 	data.station.Name &&
+	// 	data.station.Osoite &&
+	// 	data.station.Adress &&
+	// 	data.station.Kaupunki &&
+	// 	data.station.Stad &&
+	// 	data.station.Operaattor &&
+	// 	data.station.Kapasiteet &&
+	// 	data.station.x &&
+	// 	data.station.y
+	// ) {
+	if (
+		station.ID &&
+		station.Nimi &&
+		station.Namn &&
+		station.Name &&
+		station.Osoite &&
+		station.Adress &&
+		station.Kaupunki &&
+		station.Stad &&
+		station.Operaattor &&
+		station.Kapasiteet &&
+		station.x &&
+		station.y
+	) {
+		try {
+			const result = await db.query(
+				`INSERT INTO stations (station_id, nimi, namn, name, osoite, adress, kaupunki, stad, operaattor, kapasiteet, x, y) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+				[
+					station.ID,
+					station.Nimi,
+					station.Namn,
+					station.Name,
+					station.Osoite,
+					station.Adress,
+					station.Kaupunki,
+					station.Stad,
+					station.Operaattor,
+					station.Kapasiteet,
+					station.x,
+					station.y,
+				]
+			);
+			if (result.rowCount === 1) {
+				res.status(201).json(result.rowCount);
+			} else {
+				res.status(200).json({ error: 'Could not add station' });
+			}
+		} catch (e) {
+			res.status(200).json({ error: 'Could not add station' });
+		}
 	}
 });
 
--- city-bike-app/server/postgres/setup.sql
+++ city-bike-app/server/postgres/setup.sql
@@ -1,27 +1,26 @@
 CREATE TABLE IF NOT EXISTS stations (
-  id SERIAL NOT NULL PRIMARY KEY,
-	fid VARCHAR(1000),
-	station_id VARCHAR(1000) NOT NULL,
-  nimi VARCHAR(1000),
-  namn VARCHAR(1000),
-  name VARCHAR(1000) NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	station_id VARCHAR(1000) NOT NULL UNIQUE,
+	nimi VARCHAR(1000),
+	namn VARCHAR(1000),
+	name VARCHAR(1000) NOT NULL,
 	osoite VARCHAR(1000) NOT NULL,
 	adress VARCHAR(1000),
 	kaupunki VARCHAR(1000),
 	stad VARCHAR(1000),
 	operaattor VARCHAR(1000),
-	kapasiteet INT NOT NULL,
-	x NUMERIC NOT NULL,
-	y NUMERIC NOT NULL
+	kapasiteet VARCHAR(1000),
+	x VARCHAR(1000),
+	y VARCHAR(1000)
 );
 
 CREATE TABLE IF NOT EXISTS journeys (
-  id SERIAL NOT NULL PRIMARY KEY,
+	id SERIAL NOT NULL PRIMARY KEY,
 	departure_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	return_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  departure_station_id VARCHAR(1000) NOT NULL,
-  departure_station_name VARCHAR(1000) NOT NULL,
-  return_station_id VARCHAR(1000) NOT NULL,
+  	departure_station_id VARCHAR(1000) NOT NULL,
+  	departure_station_name VARCHAR(1000) NOT NULL,
+  	return_station_id VARCHAR(1000) NOT NULL,
 	return_station_name VARCHAR(1000) NOT NULL, 
 	covered_distance_meters INT DEFAULT 0,
 	duration_seconds INT DEFAULT 0
