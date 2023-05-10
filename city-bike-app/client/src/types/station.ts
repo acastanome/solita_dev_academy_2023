119141c9f38f8a9b6811bf1927e89552ce7f0c4a
Alba Castano <acastanome@gmail.com>
2023-05-10 14:14:45 +0300

    added station type

---
+++ city-bike-app/client/src/types/station.ts
@@ -0,0 +1,16 @@
+type Station = {
+	stationId: number;
+	nimi: string;
+	namn: string;
+	name: string;
+	osoite: string;
+	adress: string;
+	kaupunki: string | null;
+	stad: string | null;
+	operaattor: string | null;
+	Kapasiteet: number;
+	x: number;
+	y: number;
+};
+
+export default Station;
