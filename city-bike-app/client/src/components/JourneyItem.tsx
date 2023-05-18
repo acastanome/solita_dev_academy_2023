676b5737084d8689896c1e557ed511ddcc1f7b0f
Alba Castano <acastanome@gmail.com>
2023-05-18 16:54:05 +0300

    JourneyItem displays departure and return stations names, distance and duration of the journey

--- city-bike-app/client/src/components/JourneyItem.tsx
+++ city-bike-app/client/src/components/JourneyItem.tsx
@@ -1,7 +1,30 @@
 import Journey from '../types/journey';
 
 const JourneyItem: React.FC<{ journey: Journey }> = (props) => {
-	return <li>{props.journey.departureStationName}</li>;
+	const distanceKm = Math.floor(props.journey.coveredDistanceMeters / 1000);
+	const durationMin = Math.floor(props.journey.durationSeconds / 60);
+	return (
+		<li>
+			<ul>
+				<li>
+					<b>Departure station name: </b>
+					{props.journey.departureStationName}
+				</li>
+				<li>
+					<b>Return station name:</b>
+					{props.journey.returnStationName}
+				</li>
+				<li>
+					<b>Distance km: </b>
+					{distanceKm}
+				</li>
+				<li>
+					<b>Duration min: </b>
+					{durationMin}
+				</li>
+			</ul>
+		</li>
+	);
 };
 
 export default JourneyItem;
