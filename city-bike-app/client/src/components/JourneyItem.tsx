559eeb7a61dfd96a2205af44036f4bc20b1fe841
Alba Castano <acastanome@gmail.com>
2023-05-26 23:18:08 +0300

    For each journey, display distance in km with 2 decimals, and duration in minutes with seconds

--- city-bike-app/client/src/components/JourneyItem.tsx
+++ city-bike-app/client/src/components/JourneyItem.tsx
@@ -1,10 +1,17 @@
 import Journey from '../types/journey';
 
 const JourneyItem: React.FC<{ journey: Journey }> = (props) => {
-	const distanceKm = Math.floor(props.journey.coveredDistanceMeters / 1000);
+	const distanceKm = (props.journey.coveredDistanceMeters / 1000).toFixed(2);
 	const durationMin = Math.floor(props.journey.durationSeconds / 60);
+	const durationExtraSeconds = (
+		props.journey.durationSeconds % 60
+	).toLocaleString('en-US', {
+		minimumIntegerDigits: 2,
+		useGrouping: false,
+	});
+
 	return (
-		<li className='max-w-screen-md h-max text-blue-950 py-2 px-4 rounded border md:border-blue-300 hover:bg-blue-300'>
+		<li className='max-w-screen-md h-max text-blue-950 py-2 px-4 rounded border md:border-blue-300 hover:bg-blue-100'>
 			<ul>
 				<li>
 					<b>Departure station name: </b>
@@ -20,7 +27,7 @@ const JourneyItem: React.FC<{ journey: Journey }> = (props) => {
 				</li>
 				<li>
 					<b>Duration min: </b>
-					{durationMin}
+					{durationMin}:{durationExtraSeconds}
 				</li>
 			</ul>
 		</li>
--- city-bike-app/client/src/components/JourneysList.tsx
+++ city-bike-app/client/src/components/JourneysList.tsx
@@ -3,7 +3,7 @@ import JourneyItem from './JourneyItem';
 
 const JourneysList: React.FC<{ journeys: Journey[] }> = (props) => {
 	return (
-		<ul className='flex flex-col space-y-1 p-1'>
+		<ul className='flex flex-col space-y-1 p-1 self-center'>
 			{props.journeys.map((j, index) => (
 				<JourneyItem journey={j} key={index}></JourneyItem>
 			))}
