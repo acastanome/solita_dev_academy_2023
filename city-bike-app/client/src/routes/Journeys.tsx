e80052224f2e1b75c4517b4c6f73eef081ba5c2c
Alba Castano <acastanome@gmail.com>
2023-05-29 22:19:26 +0300

    Added search and filtering by departure station to Journeys

--- city-bike-app/client/src/routes/Journeys.tsx
+++ city-bike-app/client/src/routes/Journeys.tsx
@@ -4,33 +4,36 @@ import journeysService from '../services/journeys';
 import Journey from '../types/journey';
 import QueryParams from '../types/queryParams';
 import Pagination from '../components/Pagination';
+import SearchJourneys from '../components/SearchJourneys';
+import { toast } from 'react-hot-toast';
 
 const Journeys = () => {
 	const [page, setPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(1);
-	const [allJourneys, setAllJourneys] = useState<Journey[]>([]);
+	const [allFilteredJourneys, setAllFilteredJourneys] = useState<Journey[]>([]);
 	const [sortAndFilter, setSortAndFilter] = useState<QueryParams>({
-		// station: '',
-		// journey: '',
 		query_term: '',
+		station: '',
 		limit: 20,
-		// sort_by: 'departure_station', //departure_station, return_station, covered_distance, duration_seconds
-		// order_by: 'desc',
 	});
 
 	useEffect(() => {
-		const getAllJourneys = async () => {
+		const getAllFilteredJourneys = async () => {
 			const journeyData = await journeysService.getFilteredJourneys(
 				sortAndFilter
 			);
 			if (journeyData?.rowCount > 0) {
-				setAllJourneys(journeyData.rows);
+				setAllFilteredJourneys(journeyData.rows);
 				setTotalPages(
 					Math.ceil(journeyData.rows[0].full_count / sortAndFilter.limit)
 				);
+			} else {
+				toast.error('No journeys matched');
+				setTotalPages(1);
+				setAllFilteredJourneys([]);
 			}
 		};
-		getAllJourneys();
+		getAllFilteredJourneys();
 	}, [sortAndFilter]);
 
 	const handleNextClick = async () => {
@@ -39,7 +42,7 @@ const Journeys = () => {
 			sortAndFilter
 		);
 		if (newJourneyData?.rowCount > 0) {
-			setAllJourneys(newJourneyData.rows);
+			setAllFilteredJourneys(newJourneyData.rows);
 			setPage((prev) => prev + 1);
 			window.scrollTo({ top: 0 });
 		}
@@ -51,123 +54,26 @@ const Journeys = () => {
 			sortAndFilter
 		);
 		if (newJourneyData?.rowCount > 0) {
-			setAllJourneys(newJourneyData.rows);
+			setAllFilteredJourneys(newJourneyData.rows);
 			setPage((prev) => prev - 1);
 			window.scrollTo({ top: 0 });
 		}
 	};
 
+	const handleSearch = (params: QueryParams) => {
+		setPage(1);
+		setSortAndFilter(params);
+	};
+
 	return (
-		<div className='flex flex-col h-screen'>
-			{/* <div className='pt-16 flex flex-col items-center'>
-				<div className='flex flex-col items-center mb-20 p-8 md:px-24 md:py-24 text-white border-2 border-white rounded-2xl w-10/12 md:w-3/4'>
-					<h1 className='text-5xl font-bold'>search</h1>
-
-					<div className='flex flex-col lg:flex-row lg:gap-6 mt-16 w-full xl:max-w-3xl'>
-						<label
-							htmlFor='genre'
-							className='block text-sm font-medium text-white mb-2'
-						>
-							genre
-						</label>
-						<select
-							onChange={(e) => setGenre(e.target.value)}
-							defaultValue=''
-							id='genre'
-							className='bg-transparent border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-						>
-							<option value=''>choose genre</option>
-							{genreList.map((genre) => (
-								<option key={genre} value={genre}>
-									{genre}
-								</option>
-							))}
-						</select>
-
-						<label
-							htmlFor='minimum_rating'
-							className='block text-sm font-medium text-white mt-6 lg:mt-0 mb-2'
-						>
-							minimum rating
-						</label>
-						<select
-							onChange={(e) => setRating(e.target.value)}
-							defaultValue='0'
-							id='minimum_rating'
-							className='bg-transparent border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-						>
-							<option value=''>choose value</option>
-							<option value='0'>0</option>
-							<option value='1'>1</option>
-							<option value='2'>2</option>
-							<option value='3'>3</option>
-							<option value='4'>4</option>
-							<option value='5'>5</option>
-							<option value='6'>6</option>
-							<option value='7'>7</option>
-							<option value='8'>8</option>
-							<option value='9'>9</option>
-						</select>
-
-						<label
-							htmlFor='sort_by'
-							className='block text-sm font-medium text-white mt-6 lg:mt-0 mb-2'
-						>
-							sort by
-						</label>
-						<select
-							onChange={(e) => setSortBy(e.target.value)}
-							defaultValue='title'
-							id='sort_by'
-							className='bg-transparent border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-						>
-							<option value='like_count'>like count</option>
-							<option value='title'>mov title</option>
-							<option value='year'>mov year</option>
-							<option value='rating'>mov rating</option>
-						</select>
-
-						<label
-							htmlFor='order_by'
-							className='block text-sm font-medium text-white mt-6 lg:mt-0 mb-2'
-						>
-							order by
-						</label>
-						<select
-							onChange={(e) => setOrderBy(e.target.value)}
-							defaultValue='desc'
-							id='order_by'
-							className='bg-transparent border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-						>
-							<option value='desc'>descending</option>
-							<option value='asc'>ascending</option>
-						</select>
-					</div>
-
-					<input
-						className='bg-transparent mt-10 border-2 p-2 border-white rounded-lg w-full lg:w-1/2'
-						type='text'
-						placeholder='Search'
-						value={search}
-						onChange={({ target }) => setSearch(target.value)}
-					/>
-
-					<button
-						onClick={handleClick}
-						className='mt-10 bg-dark-red py-2 px-6 rounded-lg cursor-pointer hover:bg-dark-red/80'
-					>
-						search
-					</button>
-				</div>
-			</div> */}
-
-			<p>List of journeys:</p>
-			<JourneysList journeys={allJourneys} />
+		<div className='flex flex-col h-max-screen'>
+			<SearchJourneys onSearch={handleSearch} searchParams={sortAndFilter} />
+			<JourneysList journeys={allFilteredJourneys} />
 			<Pagination
 				page={page}
 				totalPages={totalPages}
-				handleNextClick={handleNextClick}
-				handlePreviousClick={handlePreviousClick}
+				onNextClick={handleNextClick}
+				onPreviousClick={handlePreviousClick}
 			/>
 		</div>
 	);
--- city-bike-app/server/controllers/journeys.js
+++ city-bike-app/server/controllers/journeys.js
@@ -9,38 +9,77 @@ journeysRouter.post('/page/:page', async (req, res) => {
 	const params = req.body;
 	let limit = 20;
 	let offset = 0;
+	let data = '';
 
 	if (params.limit) limit = params.limit;
 	if (req.params.page > 0) offset = limit * (req.params.page - 1);
 	try {
-		const data = await db.query(
-			`SELECT id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
- AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds" FROM journeys LIMIT $1 OFFSET $2`,
-			[limit, offset]
-		);
+		if (params.query_term) {
+			data = await db.query(
+				`SELECT
+				id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
+	 AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
+	 FROM journeys WHERE (departure_station_name || return_station_name) ILIKE ($2) LIMIT $1 OFFSET $3`,
+				[limit, '%' + params.query_term + '%', offset]
+			);
+		} else if (params.station) {
+			data = await db.query(
+				`SELECT
+				id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
+	 AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
+	 FROM journeys WHERE departure_station_name = $2 LIMIT $1  OFFSET $3`,
+				[limit, params.station, offset]
+			);
+		} else {
+			data = await db.query(
+				`SELECT
+				id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
+	 AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
+	 FROM journeys
+	 LIMIT $1 OFFSET $2`,
+				[limit, offset]
+			);
+		}
 		res.status(200).json(data);
 	} catch (e) {
 		console.log(e);
 	}
 });
 
-// get all journeys from db
+// get all filtered journeys from db
 journeysRouter.post('/', async (req, res) => {
 	const params = req.body;
 	let limit = 20;
-	let offset = 0;
+	let data = '';
 
 	if (params.limit) limit = params.limit;
 	try {
-		const data = await db.query(
-			`SELECT count(*) OVER() AS full_count,
-			id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
- AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
- FROM journeys
- LIMIT $1
- OFFSET $2`,
-			[limit, offset]
-		);
+		if (params.query_term) {
+			data = await db.query(
+				`SELECT count(*) OVER() AS full_count,
+				id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
+	 AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
+	 FROM journeys WHERE (departure_station_name || return_station_name) ILIKE ($2) LIMIT $1`,
+				[limit, '%' + params.query_term + '%']
+			);
+		} else if (params.station) {
+			data = await db.query(
+				`SELECT count(*) OVER() AS full_count,
+				id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
+	 AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
+	 FROM journeys WHERE departure_station_name = $2 LIMIT $1`,
+				[limit, params.station]
+			);
+		} else {
+			data = await db.query(
+				`SELECT count(*) OVER() AS full_count,
+				id, departure_time AS departure, return_time AS return, departure_station_id AS "departureStationId", departure_station_name AS "departureStationName", return_station_id
+	 AS "returnStationId", return_station_name AS "returnStationName", covered_distance_meters AS "coveredDistanceMeters", duration_seconds AS "durationSeconds"
+	 FROM journeys
+	 LIMIT $1`,
+				[limit]
+			);
+		}
 		res.status(200).json(data);
 	} catch (e) {
 		console.log(e);
