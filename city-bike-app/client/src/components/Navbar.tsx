3dd82dc206612271e27c26e3edec281c8a207ca0
Alba Castano <acastanome@gmail.com>
2023-05-29 20:37:47 +0300

    css changes to Navbar buttons

--- city-bike-app/client/src/components/Navbar.tsx
+++ city-bike-app/client/src/components/Navbar.tsx
@@ -11,15 +11,15 @@ const Navbar = () => {
 		navigate('/stations');
 	};
 	return (
-		<div className='flex flex-row md:space-x-1 md:p-1'>
+		<div className='flex flex-row space-x-1 p-1'>
 			<button
-				className='w-32 h-16 text-blue-950 font-bold py-2 px-4 rounded border md:border-blue-300 hover:bg-blue-300'
+				className='w-32 md:h-16 text-blue-950 font-bold py-2 px-4 rounded border md:border-blue-300 hover:bg-blue-300'
 				onClick={navigateJourneys}
 			>
 				Journeys
 			</button>
 			<button
-				className='w-32 h-16 text-blue-950 font-bold py-2 px-4 rounded border md:border-blue-300 hover:bg-blue-300'
+				className='w-32 md:h-16 text-blue-950 font-bold py-2 px-4 rounded border md:border-blue-300 hover:bg-blue-300'
 				onClick={navigateStations}
 			>
 				Stations
