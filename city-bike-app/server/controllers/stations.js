723f343954a879932a317c58ab2c5712a8045611
Alba Castano <acastanome@gmail.com>
2023-05-31 19:20:29 +0300

    Removed adding stations endpoint

--- city-bike-app/server/controllers/stations.js
+++ city-bike-app/server/controllers/stations.js
@@ -1,7 +1,6 @@
 const db = require('../db');
 const { addStationToDb } = require('../utils/addStationToDb');
 const { getStationsFromFile } = require('../utils/getStationsFromFile');
-const { validateSatation } = require('../utils/validateStation');
 
 const stationsRouter = require('express').Router();
 
@@ -154,34 +153,6 @@ stationsRouter.post('/', async (req, res) => {
 		console.log(e);
 	}
 });
-
-// // TODO: for now takes example station from file, change to station from request
-// stationsRouter.post('/addStation', async (req, res) => {
-// 	try {
-// 		const allStations = await getStationsFromFile(
-// 			'./assets/bike_stations_data/Helsingin_ja_Espoon_asemat_avoin.csv'
-// 		);
-// 		// const data = req.body;
-// 		// console.log(allStations[5]);
-// 		const station = await allStations.filter((s) => s.ID === 733);
-// 		const valid = await validateSatation(station[0]);
-// 		if (valid === true) {
-// 			const result = await addStationToDb(station[0]);
-// 			if (result.error) {
-// 				res.status(400).json({ error: result.error });
-// 			} else if (result === 1) {
-// 				res.status(201).json({ msg: 'Station added succesfully' });
-// 			} else {
-// 				res.status(200).json(result);
-// 			}
-// 		} else {
-// 			res.status(400).json({ error: 'Could not add station, missing content' });
-// 		}
-// 	} catch (e) {
-// 		console.log(e);
-// 		res.status(400).json({ error: 'Station could not be added.' });
-// 	}
-// });
 
 stationsRouter.get('/populateDatabase', async (req, res) => {
 	try {
